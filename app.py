87addad15eebd13f1cc2674dcbe64108f4fef22b
bhernandez333 <61214858+bhernandez333@users.noreply.github.com>
2020-03-08 22:24:17 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 @app.route('/')
 def Sumatoria():
     N1 = request.args.get("Numero1", 0, type=int)
-    N2 = request.args.get("Numero2", 0, type=int )
+    N2 = request.args.get("Numero2", 0, type=int)
 #    return f'Hello,{escape(name)}!'
     return 'Resultado de la suma de ambos valores ==> {}'.format(N1 + N2)
