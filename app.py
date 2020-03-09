c67085531768320fa55e44d1a8069e3d1aa7d56e
bhernandez333 <61214858+bhernandez333@users.noreply.github.com>
2020-03-08 22:17:50 -0600

    Initial commit

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, escape, request
+app = Flask(__name__)
+
+@app.route('/')
+def Sumatoria():
+    N1 = request.args.get("Numero1", 0, type=int)
+    N2 = request.args.get("Numero2", 0, type=int)
+#    return f'Hello,{escape(name)}!'
+    return 'Resultado de la suma de ambos valores ==> {}'.format(N1 + N2)
